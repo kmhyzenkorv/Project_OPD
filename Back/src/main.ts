0882a208c2ae7216e5ad1f384bb50d378423552f
MementoSH <diwepanricardo@gmail.com>
2025-04-29 17:45:03 +0500

    cors

--- Back/src/main.ts
+++ Back/src/main.ts
@@ -17,6 +17,10 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/api/docs', app, document);
 
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
   await app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server started on port = ${PORT}`);
   });
